a682c243f2f7ff96d604841a8a3d3d90158dfdb4
Srikar Manikonda <avatsara3@gmail.com>
2020-05-26 17:51:15 -0500

    setup navigation

--- App.js
+++ App.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import login from './components/Login.js'
+import signed from './components/Signup.js'
 import { View, StyleSheet, Text, TouchableWithoutFeedback, Keyboard, Image, TextInput, TouchableOpacity, Dimensions, AsyncStorage, KeyboardAvoidingView } from 'react-native';
-
+import { createStackNavigator } from 'react-navigation-stack'
+import { createAppContainer } from 'react-navigation'
 const height = Dimensions.get('window').height;
 const width = Dimensions.get('window').width;
-//const wid = width / 380;
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
 
+
+const AppNavigator = createStackNavigator({
+        Login: {
+          screen: log
+        },
+
+        Signup: {
+          screen:signed
+        }
+      })
+
+
+const AppContainer = createAppContainer(AppNavigator);
+      return(
+      <AppContainer/>
+      );
+  }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
