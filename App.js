27e2b46a37bdef1404109a9f372f1e5bdccfb3f0
Srikar Manikonda <avatsara3@gmail.com>
2020-05-26 19:04:28 -0500

    basics

--- App.js
+++ App.js
@@ -1,17 +1,72 @@
-import React from 'react';
+import React, {Component, useState} from 'react';
 import { View, StyleSheet, Text, TouchableWithoutFeedback, Keyboard, Image, TextInput, TouchableOpacity, Dimensions, AsyncStorage, KeyboardAvoidingView } from 'react-native';
-
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 const height = Dimensions.get('window').height;
 const width = Dimensions.get('window').width;
-//const wid = width / 380;
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+class Login extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      username:'',
+      password:'',
+    }
+    Text.defaultProps = Text.defaultProps || {};
+    // Ignore dynamic type scaling on iOS
+    Text.defaultProps.allowFontScaling = false;
 
+  }
+  render(){
+
+  return (
+      <View style={styles.container}>
+          <View style={{ width: '100%', height: '80%', alignItems: 'flex-end' }}>
+            <View style={{
+              width: '80%',
+              flex: 1.5,
+              borderColor: '#3C5984',
+              borderWidth: 2,
+              borderRadius: 20,
+            }}>
+              <TextInput                style={{ fontSize: 18, width: '95%', height: '100%', marginLeft: '5%', fontFamily: 'Arial' }}
+                autoCapitalize='none'
+                autoCompleteType='off'
+                placeholder="Username"
+                keyboardType={Platform.OS === 'ios' ? 'ascii-capable' : 'visible-password'}
+                onChangeText={(value) => this.setState({ username: value })}
+                value={this.state.username}
+
+              /></View>
+            <View style={{ width: '100%', flex: 0.4 }}></View>
+            <View style={{
+              width: '100%',
+              flex: 1.5,
+              borderColor: '#3C5984',
+              borderWidth: 2,
+              borderRadius: 20
+            }}>
+              <TextInput
+                style={{ fontSize: 18, width: '95%', height: '100%', marginLeft: '5%', fontFamily: 'Arial' }}
+                autoCapitalize='none'
+                autoCompleteType='off'
+                placeholder="Password"
+                onChangeText={(value) => this.setState({ password: value })}
+                value={this.state.password}
+                secureTextEntry={true}
+
+              />
+              </View>
+
+          </View>
+        </View>
+  )
+  }
+  }
+
+  const AppNavigator = createSwitchNavigator({
+    Home: {
+      screen: Login
+    },
+})
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -20,3 +75,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+
+export default createAppContainer(AppNavigator);
