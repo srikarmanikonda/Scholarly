59b8bd1e3ffcdfcae4dc05585912cad26bc7e44f
danielzhuang11 <35277594+danielzhuang11@users.noreply.github.com>
2020-10-18 11:49:23 -0500

    added scholarships

--- screens/Scholarships.js
+++ screens/Scholarships.js
@@ -17,24 +17,67 @@ let first = true;
 let first2 = true;
 
 var  predata  = [{
-    name: "Srikar Manikonda $1000 grant",
-    id: "100",
-    link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-    description: " $1000 to the person that can draw the best portrait of mr. healey",
-    deadline:"today"
-  },{
-    name: "test",
-    id: "300",
-    link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-    description: " test",
-    deadline:"tommorrow"
-  },{
-    name: "hello",
-    id: "9999",
-    link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-    description: " stahp",
-    deadline:"tommorrow"
-  }]
+  name: "Order Sons of Italy in America National Leadership Grant",
+  id: "100",
+  link: "https://www.osia.org/students/scholarships.php",
+  description: " $25,000  Applicant must be of Italian heritage and be a full-time student attending or planning on attending an accredited four-year institution who has demonstrated exceptional leadership qualities and a distinguished level of scholastic achievement. Recipients will be officially recognized in May at the SIF's National Education & Leadership Awards (NELA) Gala in Washington, DC. Monetary awards are presented directly to students' academic institutions in accordance with eligibility requirements.",
+  deadline:" February 28 "
+},{
+  name: "Agnes Jones Jackson Scholarship",
+  id: "300",
+  link: "https://www.naacp.org/",
+  description: " $2,500  Applicant must be current member of the NAACP and must not have reached the age of 25 by the application deadline. Applicant must be a citizen of the United States and be enrolled in an accredited college or university in the United States. Undergraduate students must be full-time with a GPA of at least 2.5 (C+). Graduate students may be full or part-time with a GPA of 3.0 (B). Applicant must demonstrate financial need. Renewal of the Agnes Jones Jackson Scholarship is competitive. Please refer to the NAACP National Website or call the NAACP National Education Department at 410 580-5760 for more information.",
+  deadline:" March 31 "
+},{
+  name: "Fellowship in Jewish Studies",
+  id: "400",
+  link: "http://www.mfjc.org/support/howto.html",
+  description: " $10,000  Applicant must have an interest in Jewish studies. Award is to assist well-qualified individuals in carrying out an independent scholarly, literary, or art project, in a field of Jewish specialization which makes a significant contribution to the understanding, preservation, enhancement, or transmission of Jewish culture.",
+  deadline:" October 31 "
+},{
+  name: "Billy Smith Memorial Scholarship",
+  id: "500",
+  link: "https://www.scholarships.com/financial-aid/college-scholarships/sports-scholarships/baseball-scholarships/billy-smith-memorial-scholarship/#",
+  description: " The Billy Smith Memorial Scholarship provides educational resources for graduation high school baseball or softball players who intend to pursue a sports-related college degree. Applicant must be a graduating high school senior, reside in Winnebago, Boone, Ogle or Stephenson County, have played at least three years of baseball or softball in high school, including senior year, must major in a sports-related field in college, and must have a minimum GPA of 3.0. For more information or to apply, please visit the scholarship provider's website. ",
+  deadline:"February 01 "
+},{
+  name: "Oratorical Speech Scholarship Contest",
+  id: "600",
+  link: "http://www.mfjc.org/support/howto.html",
+  description: " $1000  Applicant must be a finalist in the oratorical contest.",
+  deadline:"First Week of January"
+},{
+  name: "Verne R. Dapp Memorial Scholarship",
+  id: "700",
+  link: "https://www.ans.org/",
+  description: " $3,000 Applicant must be a U.S. citizen or permanent resident enrolled in a course of study relating to a degree in nuclear science and technology. Applicant must be sponsored by an ANS local section, division, student branch, committee member or organization member.",
+  deadline:" February 1 "
+},{
+  name: "Dental Student Scholarships",
+  id: "800",
+  link: "http://www.mfjc.org/support/howto.html",
+  description: " $2,500  Applicant must be a U.S. citizen and have a minimum 3.0 GPA. Selection is based upon financial need, academic achievement, and personal and professional goals. Two reference forms from two dental school representatives (i.e., professor or academic advisor) in support of the application must be submitted as part of the application form.",
+  deadline:" November 11 "
+},{
+  name: "Navy Supply Corps Foundation Scholarship",
+  id: "900",
+  link: "https://www.usnscf.com/Foundation/pages/Programs/Scholarship",
+  description: " $2,500  Applicant must be the child of a Navy Supply Corps Officer (including Warrant) or associated supply enlisted ratings on active duty, in reserve status, retired-with-pay, or deceased, and have a minimum 3.0 GPA. Selection is based upon scholastic ability, character, leadership, and financial need.",
+  deadline:" April 10 "
+},{
+  name: "Sarah Bradley Tyson Memorial Fellowship",
+  id: "950",
+  link: "https://wnfga.org/scholarships/fellowships/",
+  description: " $1,000   Applicant must be a U.S. citizen and have a minimum 3.0 GPA. Selection is based upon financial need, academic achievement, and personal and professional goals. Two reference forms from two dental school representatives (i.e., professor or academic advisor) in support of the application must be submitted as part of the application form.",
+  deadline:"January 20"
+},{
+  name: "Suburban Hospital Casey Scholarships",
+  id: "970",
+  link: "https://www.hopkinsmedicine.org/suburban_hospital/about_the_hospital/careers/education_career_development.html",
+  description: " $1,000 Applicant must be a current employee who is enrolled in a nursing or allied health (e.g. radiology technology, respiratory therapy, medical technology, physical therapy, physical therapy assistant, occupational therapy, or physician assistant) undergraduate program or a non-employee who is enrolled in a nursing program leading to a B.S.N. or higher. Minimum 2.5 GPA, two letters of recommendation, transcript, and letter of acceptance/good standing required. Award requires a work commitment at Suburban Hospital in Bethesda, MD, upon completion of degree program.",
+  deadline:"November 30; April 30"
+}]
+
 
 var fullData=  [];
 
